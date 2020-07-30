20f9a260c720e13f63295f1498a2ce524ca6c98a
ingter <gktmdgh6042@naver.com>
2020-07-30 10:49:41 +0900

    tutorial

---
+++ secondapp/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('main/', views.main, name='main'),
+    path('show/', views.show, name='show'),
+]
---
+++ secondapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Hospital
+
+def show(request):
+    hospital = Hospital.objects.all()
+    #return render(request, 'show.html', {'list':hospital})
+    html=''
+    for h in hospital:
+        html += '%s %s %s %s %s %s %s <br><br>' %(h.id, h.sido, h.name, h.medical, h.room, h.tel, h.address) 
+    return HttpResponse(html)
+
+def index1(request):
+    return HttpResponse('index1')
+
+def main(request):
+    return HttpResponse('main!')
+# Create your views here.
